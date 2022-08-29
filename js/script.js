2c5b82781f9c5cd8c65310fbffe0a4ed58d0178e
Lukasz Gorka <lukaszgorka@hotmail.com>
2022-08-29 11:37:51 +0200

    clear TitleList

--- js/script.js
+++ js/script.js
@@ -44,6 +44,40 @@ const titleClickHandler = function(event) {
 
   targetArticle.classList.add('active');
 
+  const optArticleSelector = '.post',
+    optTitleSelector = '.post-title',
+    optTitleListSelector = '.titles';
+  
+  function generateTitleLinks() {
+
+    /* remove contents of titleList */
+    
+    const titleList = document.querySelector(optTitleListSelector);
+    
+    function clearTitleList(){
+      document.querySelector(optTitleListSelector).innerHTML = '';
+    }
+
+    clearTitleList();
+    
+    console.log(optTitleListSelector)
+    
+
+    /* for each article */
+
+      /* get the article id */
+
+      /* find the title element */
+
+      /* get the title from the title element */
+
+      /* create HTML of the link */
+
+      /* insert link into titleList */
+  }
+
+  generateTitleLinks();
+  
 }
 
 const links = document.querySelectorAll('.titles a');
